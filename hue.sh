304e4067e99f67435c02a67ef0c8b9532d9637d7
Andy Watson <andy_watson@uk.ibm.com>
2018-11-30 14:14:37 +0000

    upload files

---
+++ hue.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#*******************************************************************************
+# Licensed Materials - Property of IBM
+# "Restricted Materials of IBM"
+#
+# Copyright IBM Corp. 2017 All Rights Reserved
+#
+# US Government Users Restricted Rights - Use, duplication or disclosure
+# restricted by GSA ADP Schedule Contract with IBM Corp.
+#
+while true 
+do
+node hue.js White
+sleep 3
+node hue.js Yellow
+sleep 3
+node hue.js Orange
+sleep 3
+node hue.js Red
+sleep 3
+node hue.js Blue
+sleep 3
+node hue.js Green
+sleep 3
+node hue.js Purple
+sleep 5
+done
